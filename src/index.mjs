699f3c4c5d12451653f28d3f1901a7626f297111
pablo-conte <pablo.correa.nr@gmail.com>
2022-10-03 23:49:27 -0300

    Feat -> Using nodemon to run the server

--- src/index.mjs
+++ src/index.mjs
@@ -8,6 +8,11 @@ app.get("/ignite", (request, response) => {
     return response.json({message: "Hello world Ignite!"})
 })
 
+app.get("/pablo", (request, response) => {
+    return response.json({message: "Hello world Sir, how can I help you?!"})
+})
+
+
 
 app.listen(8000, () => {
     console.log("O servidor está rodando no endereço: localhost:8000")
