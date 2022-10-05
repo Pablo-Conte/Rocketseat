eff0e204bb4731e82c1819b01959c0d528cf72f6
pablo-conte <pablo.correa.nr@gmail.com>
2022-10-05 01:24:49 -0300

    Feat -> adding new routes + insomnia

--- src/index.mjs
+++ src/index.mjs
@@ -12,6 +12,54 @@ app.get("/pablo", (request, response) => {
     return response.json({message: "Hello world Sir, how can I help you?!"})
 })
 
+app.get("/courses", (request, response) => {
+    return response.json({
+        "Curso 1": "Informática",
+        "Curso 2": "Eletromecânica",
+        "Curso 3": "Eletrotécnica",
+        "Curso 4": "Agronomia"
+    })
+})
+
+app.post("/courses", (request, response) => {
+    return response.json({
+        "Curso 1": "Informática",
+        "Curso 2": "Eletromecânica",
+        "Curso 3": "Eletrotécnica",
+        "Curso 4": "Agronomia",
+        "Curso 5": "Medicina"
+    })
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json({
+        "Curso 6": "Informática",
+        "Curso 2": "Eletromecânica",
+        "Curso 3": "Eletrotécnica",
+        "Curso 4": "Agronomia",
+        "Curso 5": "Medicina"
+    })
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json({
+        "Curso 6": "Informática",
+        "Curso 7": "Eletromecânica",
+        "Curso 3": "Eletrotécnica",
+        "Curso 4": "Agronomia",
+        "Curso 5": "Medicina"
+    })
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json({
+        "Curso 6": "Informática",
+        "Curso 7": "Eletromecânica",
+        "Curso 4": "Agronomia",
+        "Curso 5": "Medicina"
+    })
+})
+
 
 
 app.listen(8000, () => {
